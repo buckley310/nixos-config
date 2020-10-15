1b243378695a18d8f1fcd91d1766cd29fe58a9ae
Sean Buckley <sean.bck@gmail.com>
2020-10-15 11:39:38 -0400

    remove old dash-to-panel script

--- modules/gnome/dash-to-panel.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-dconf write /org/gnome/shell/extensions/dash-to-panel/panel-size 40
-dconf write /org/gnome/shell/extensions/dash-to-panel/group-apps false
-dconf write /org/gnome/shell/extensions/dash-to-panel/isolate-workspaces true
-dconf write /org/gnome/shell/extensions/dash-to-panel/show-window-previews false
