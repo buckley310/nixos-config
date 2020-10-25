b3c48988cc5f030a0462589e180f8bd5ada537fa
Sean Buckley <sean.bck@gmail.com>
2020-10-25 04:28:55 -0400

    add "status-on-console" module

---
+++ modules/status-on-console/default.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+let
+  ncfg = pkgs.writeText "neofetch.conf" ''
+    print_info() {
+        info title
+        info underline
+
+        info "OS" distro
+        info "Host" model
+        info "Kernel" kernel
+        info "Uptime" uptime
+        info "Packages" packages
+        info "Shell" shell
+        info "Resolution" resolution
+        info "CPU" cpu
+        info "GPU" gpu
+        info "Memory" memory
+        info "Disk" disk
+        info "Local IP" local_ip
+        # info "Public IP" public_ip
+        info "Users" users
+
+        info cols
+    }
+  '';
+
+  nscript = pkgs.writeShellScript "neofetch-wrapped" ''
+    export PATH="$PATH:${pkgs.iproute}/bin:${pkgs.gawk}/bin"
+    (
+      ${pkgs.neofetch}/bin/neofetch --config "${ncfg}"
+      echo '\l'
+    ) >/run/issue
+  '';
+
+in
+{
+  environment.etc.issue.source = pkgs.lib.mkForce "/run/issue";
+  systemd.services."getty@".serviceConfig.ExecStartPre = nscript;
+}
