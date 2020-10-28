d97c1f87276136fb96fa411cd6bdab1c62ef3f34
Sean Buckley <sean.bck@gmail.com>
2020-10-28 14:42:11 -0400

    remove UID from users

--- modules/sean/default.nix
+++ modules/sean/default.nix
@@ -3,7 +3,6 @@
   users.users = {
     sean = {
       isNormalUser = true;
-      uid = 1000;
       extraGroups = [ "wheel" "audio" "video" "networkmanager" "dialout" "input" "wireshark" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqWHzIXaF88Y8+64gBlLbZ5ZZcLl08kTHG1clHd7gaq"
