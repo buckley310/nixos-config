981fae66628829c568565914e85baa78bc23f0a6
Sean Buckley <sean.bck@gmail.com>
2020-10-28 14:39:54 -0400

    add Dell laptop SSH key

--- modules/sean/default.nix
+++ modules/sean/default.nix
@@ -8,6 +8,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqWHzIXaF88Y8+64gBlLbZ5ZZcLl08kTHG1clHd7gaq"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALNzkn63+7jlmGk68bj03A4Ym7aBdPTODq+QHyYULfO"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtTBrVXCDelPYUeUzFSLhWtBDI8IO6HVpX4ewUxD+Nc"
       ];
     };
     test = {
