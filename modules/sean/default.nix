d33281e47c627b75fd16c8cade2af2e98b1d87ed
Sean Buckley <sean.bck@gmail.com>
2020-11-07 21:34:57 -0500

    update HP laptop SSH key

--- modules/sean/default.nix
+++ modules/sean/default.nix
@@ -6,7 +6,7 @@
       extraGroups = [ "wheel" "audio" "video" "networkmanager" "dialout" "input" "wireshark" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqWHzIXaF88Y8+64gBlLbZ5ZZcLl08kTHG1clHd7gaq"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALNzkn63+7jlmGk68bj03A4Ym7aBdPTODq+QHyYULfO"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILh1MVRPld8lg8U7j4QwurxkTGLd4EYEn+JaplqXMqNW"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtTBrVXCDelPYUeUzFSLhWtBDI8IO6HVpX4ewUxD+Nc"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLZgFlJTT8wFz2DGeB1YETKPvm63/u1kT7pzranCoqP"
       ];
