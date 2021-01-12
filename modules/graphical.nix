b52e0a6481c1ecf379176cb94bf4d02d7ead26c1
Sean Buckley <sean.bck@gmail.com>
2021-01-12 16:11:32 -0500

    remove deno extension

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -19,16 +19,6 @@
         ms-python.python
         ms-vscode.cpptools
         ms-azuretools.vscode-docker
-
-        (vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "vscode-deno";
-            publisher = "denoland";
-            version = "2.3.3";
-            sha256 = "1sirni7hamwp0dld5l8qw7jfrjxf3pvsmjrx14zvg2bwwrv4p0m6";
-          };
-        })
-
       ];
     })
 
