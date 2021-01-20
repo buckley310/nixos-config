59f488f292083b9b36a01c603c0cc2887b1d4cd4
Sean Buckley <sean.bck@gmail.com>
2021-01-20 11:06:57 -0500

    allow GC without password

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -47,6 +47,7 @@
   security.sudo.extraRules = [{
     groups = [ "wheel" ];
     commands = [
+      { options = [ "NOPASSWD" ]; command = "/run/current-system/sw/bin/nix-collect-garbage -d"; }
       { options = [ "NOPASSWD" ]; command = "/run/current-system/sw/bin/nix-channel --update"; }
       { options = [ "NOPASSWD" ]; command = "/run/current-system/sw/bin/nixos-rebuild switch"; }
       { options = [ "NOPASSWD" ]; command = "/run/current-system/sw/bin/nixos-rebuild switch --upgrade"; }
