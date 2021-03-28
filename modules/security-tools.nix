ab88ae88edb4fc85b2d45b15e20929e22f6b5c6b
Sean Buckley <sean.bck@gmail.com>
2021-03-28 02:52:09 -0400

    whitespace

--- modules/security-tools.nix
+++ modules/security-tools.nix
@@ -6,48 +6,48 @@ in
   options.sconfig.security-tools = lib.mkEnableOption "Enable security tools";
 
   config = lib.mkIf cfg {
-  environment.systemPackages = with pkgs; [
-    exiftool
-    burpsuite
-    nmap
-    masscan
-    binutils
-    remmina
-    openvpn
-    socat
-    ghidra-bin
-    wfuzz
-    gobuster
-    dirb
-    pwndbg
-    thc-hydra
-    metasploit
+    environment.systemPackages = with pkgs; [
+      exiftool
+      burpsuite
+      nmap
+      masscan
+      binutils
+      remmina
+      openvpn
+      socat
+      ghidra-bin
+      wfuzz
+      gobuster
+      dirb
+      pwndbg
+      thc-hydra
+      metasploit
 
-    (callPackage ../pkgs/binary-ninja-personal { })
+      (callPackage ../pkgs/binary-ninja-personal { })
 
-    (writeShellScriptBin "searchsploit" ''
-      set -e
-      (
-          cd ~/.cache
-          [ -e exploitdb ] || git clone https://github.com/offensive-security/exploitdb.git
-          cd exploitdb
-          if find .git -maxdepth 0 -cmin +60 | grep -q git
-          then
-              git pull
-          fi
-      )
-      exec ~/.cache/exploitdb/searchsploit "$@"
-    '')
-  ];
+      (writeShellScriptBin "searchsploit" ''
+        set -e
+        (
+            cd ~/.cache
+            [ -e exploitdb ] || git clone https://github.com/offensive-security/exploitdb.git
+            cd exploitdb
+            if find .git -maxdepth 0 -cmin +60 | grep -q git
+            then
+                git pull
+            fi
+        )
+        exec ~/.cache/exploitdb/searchsploit "$@"
+      '')
+    ];
 
-  programs = {
-    wireshark.enable = true;
-    wireshark.package = pkgs.wireshark;
-  };
+    programs = {
+      wireshark.enable = true;
+      wireshark.package = pkgs.wireshark;
+    };
 
-  users.users.sandy = {
-    isNormalUser = true;
-    isSystemUser = true;
-  };
+    users.users.sandy = {
+      isNormalUser = true;
+      isSystemUser = true;
+    };
   };
 }
