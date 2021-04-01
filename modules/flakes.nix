11feec88b729ada8469c5997cdc08b5e8c801023
Sean Buckley <sean.bck@gmail.com>
2021-03-31 23:43:23 -0400

    flakes: remove extra config

--- modules/flakes.nix
+++ modules/flakes.nix
@@ -6,12 +6,9 @@ in
   options.sconfig.flakes = lib.mkEnableOption "Enable Flakes";
 
   config = lib.mkIf cfg {
-    environment.systemPackages = [ pkgs.nixFlakes ];
-    nix = {
-      package = pkgs.nixFlakes;
-      extraOptions = ''
-        experimental-features = nix-command flakes
-      '';
-    };
+    nix.package = pkgs.nixFlakes;
+    nix.extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
   };
 }
