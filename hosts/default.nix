5bd9a8302a44aa6d97b2be2e0274a8e3be544b40
Sean Buckley <sean.bck@gmail.com>
2021-04-05 23:24:24 -0400

    move host config common modules

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,22 +1,29 @@
 { unstable, stable2009 }:
 let
+
+  commonModules = [
+    ../.
+    ({ ... }: {
+      config = {
+        sconfig.flakes.enable = true;
+        sconfig.flakes.rebuildPath = "github:buckley310/nixos-config";
+      };
+    })
+  ];
+
   mkStandardSystem = { name, pkgs }: pkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = [
+    modules = commonModules ++ [
       pkgs.nixosModules.notDetected
-      ({ ... }: { config = { sconfig.flakes.enable = true; }; })
       (./. + "/configuration_${name}.nix")
-      ../.
     ];
   };
 
   mkQemuSystem = { name, pkgs }: pkgs.lib.nixosSystem {
     system = "x86_64-linux";
-    modules = [
-      ({ modulesPath, ... }: { imports = [ "${modulesPath}/profiles/qemu-guest.nix" ]; })
-      ({ ... }: { config = { sconfig.flakes.enable = true; }; })
+    modules = commonModules ++ [
+      (x: { imports = [ "${x.modulesPath}/profiles/qemu-guest.nix" ]; })
       (./. + "/configuration_${name}.nix")
-      ../.
     ];
   };
 
