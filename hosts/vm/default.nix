5d3c58250913ea7519efb728ece2cf288c5dbf10
Sean Buckley <sean.bck@gmail.com>
2021-04-08 21:12:43 -0400

    remove sudo prompt for test vms

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -4,4 +4,5 @@
   sconfig.profile = "server";
   boot.loader.grub.device = "/dev/vda";
   fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
+  security.sudo.wheelNeedsPassword = false;
 }
