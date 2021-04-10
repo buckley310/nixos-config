bca03df8a913b8c9d6df7063ee4a3892fb486e7a
Sean Buckley <sean.bck@gmail.com>
2021-04-10 14:39:29 -0400

    add root ssh keys to test vm

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,7 +1,8 @@
-{ ... }:
+{ config, ... }:
 {
   sconfig.profile = "server";
   boot.loader.grub.device = "/dev/vda";
   fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
   security.sudo.wheelNeedsPassword = false;
+  users.users.root.openssh.authorizedKeys = config.users.users.sean.openssh.authorizedKeys;
 }
