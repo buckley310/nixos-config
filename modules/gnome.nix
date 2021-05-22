52c0a741b68b071c50c5c707fe55c9324301f140
Sean Buckley <sean.bck@gmail.com>
2021-05-22 00:30:42 -0400

    use beta dash-to-dock

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -18,7 +18,7 @@ in
     environment.systemPackages = with pkgs; [
       gnome3.gnome-tweaks
       gnomeExtensions.appindicator
-      gnomeExtensions.dash-to-dock
+      (callPackage ../pkgs/dash-to-dock { })
       gnomeExtensions.dash-to-panel
       numix-icon-theme
       qemu_kvm
