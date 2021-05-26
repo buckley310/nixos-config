e5a3879e7eee8a37ff885622b3e3c72ccc462791
Sean Buckley <sean.bck@gmail.com>
2021-05-26 00:36:00 -0400

    reduce config for test vm

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -1,10 +1,5 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   sconfig.profile = "server";
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  fileSystems."/" = { device = "/dev/disk/by-partlabel/_root"; fsType = "ext4"; };
-  fileSystems."/boot" = { device = "/dev/disk/by-partlabel/_esp"; fsType = "vfat"; };
-  security.sudo.wheelNeedsPassword = false;
-  users.users.root.openssh.authorizedKeys = config.users.users.sean.openssh.authorizedKeys;
+  users.users.root.password = "toor";
 }
