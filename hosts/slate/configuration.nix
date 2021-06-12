58aee1a57ce922f3c2bdbab06a4815eb25984b51
Sean Buckley <sean.bck@gmail.com>
2021-06-11 22:54:02 -0400

    hosts/slate: init

---
+++ hosts/slate/configuration.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  networking.search = [ "bck.me" ];
+
+  sconfig = {
+    profile = "desktop";
+    gnome = true;
+    security-tools = true;
+  };
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    initrd.luks.devices.cryptroot = { device = "/dev/disk/by-partlabel/_luks"; };
+  };
+
+  fileSystems = {
+    "/" = { device = "/dev/mapper/cryptroot"; fsType = "btrfs"; options = [ "subvol=/os" "discard" "compress=zstd" ]; };
+    "/home" = { device = "/dev/mapper/cryptroot"; fsType = "btrfs"; options = [ "subvol=/home" "discard" "compress=zstd" ]; };
+    "/boot" = { device = "/dev/disk/by-partlabel/_esp"; fsType = "vfat"; };
+  };
+
+  system.stateVersion = "21.05";
+}
