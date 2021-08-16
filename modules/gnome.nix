ab63fa6eda3397656a758c2117a6d91014ff243a
Sean Buckley <sean.bck@gmail.com>
2021-08-16 09:40:02 -0400

    gdm: default to xorg

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -13,6 +13,7 @@ in
       displayManager.gdm.enable = true;
       displayManager.gdm.autoSuspend = false;
       desktopManager.gnome.enable = true;
+      displayManager.defaultSession = "gnome-xorg";
     };
 
     services.colord.enable = false;
