dabdeb44819273561a8c879c593dc840b9d30db2
Sean Buckley <sean.bck@gmail.com>
2021-08-16 02:11:10 -0400

    install gef optional dependencies

--- pkgs/gef/default.nix
+++ pkgs/gef/default.nix
@@ -1,11 +1,26 @@
-{ stdenv, fetchFromGitHub, makeWrapper, gdb }:
+{ stdenv, lib, fetchFromGitHub, makeWrapper, gdb, python3 }:
 let
+
   src = fetchFromGitHub {
     owner = "hugsy";
     repo = "gef";
     rev = "2021.07";
     sha256 = "zKn3yS9h8bzjsb/iPgNU8g5IgXFBaKvM7osTqzzv16s=";
   };
+
+  reqs =
+    (builtins.filter (x: x != "")
+      (lib.splitString "\n"
+        (builtins.readFile "${src}/requirements.txt")));
+
+  # python3.pkgs.ropper does not work with makePythonPath. Swap it out.
+  pyp = python3.pkgs // {
+    ropper = python3.pkgs.buildPythonPackage {
+      inherit (python3.pkgs.ropper) name src propagatedBuildInputs;
+    };
+  };
+
+  optionals = pyp.makePythonPath (map (x: pyp.${x}) reqs);
 
 in
 stdenv.mkDerivation {
@@ -15,6 +30,7 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p "$out/bin"
     makeWrapper "${gdb}/bin/gdb" "$out/bin/gef" \
+        --suffix PYTHONPATH : "${optionals}" \
         --add-flags "-x ${src}/gef.py" \
         --add-flags "-ex 'gef config context.clear_screen 0'"
   '';
