61cfd34f4245c5bb0897d87c98044131c092fb0f
Sean Buckley <sean.bck@gmail.com>
2021-08-18 13:34:34 -0400

    backport postman

--- modules/security-tools.nix
+++ modules/security-tools.nix
@@ -31,6 +31,29 @@ in
       unstable.exploitdb
     ];
 
+    nixpkgs.overlays = [
+      (self: super: {
+        postman = super.postman.overrideAttrs (old: rec {
+          buildInputs = old.buildInputs ++ [ pkgs.libxkbcommon ];
+          version = "8.10.0";
+          src = super.fetchurl {
+            url = "https://dl.pstmn.io/download/version/${version}/linux64";
+            sha256 = "05f3eaa229483a7e1f698e6e2ea2031d37687de540d4fad05ce677ac216db24d";
+            name = "postman.tar.gz";
+          };
+          postFixup = ''
+            pushd $out/share/postman
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" _Postman
+            for file in $(find . -type f \( -name \*.node -o -name _Postman -o -name \*.so\* \) ); do
+              ORIGIN=$(patchelf --print-rpath $file); \
+              patchelf --set-rpath "${lib.makeLibraryPath buildInputs}:$ORIGIN" $file
+            done
+            popd
+          '';
+        });
+      })
+    ];
+
     programs = {
       wireshark.enable = true;
       wireshark.package = pkgs.wireshark;
