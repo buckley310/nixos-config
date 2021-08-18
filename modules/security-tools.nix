f07a8fbe7653527b369f0617637c440a75a63c4f
Sean Buckley <sean.bck@gmail.com>
2021-08-18 17:54:23 -0400

    security-tools: add proxybrowser script

--- modules/security-tools.nix
+++ modules/security-tools.nix
@@ -1,6 +1,25 @@
 { config, pkgs, lib, ... }:
 let
+
   cfg = config.sconfig.security-tools;
+
+  proxybrowser = pkgs.writeShellScriptBin "proxybrowser" ''
+    exec ${pkgs.ungoogled-chromium}/bin/chromium \
+    --disable-background-networking \
+    --disable-default-apps \
+    --disable-plugins-discovery \
+    --disk-cache-size=0 \
+    --ignore-certificate-errors \
+    --no-default-browser-check \
+    --no-experiments \
+    --no-first-run \
+    --no-pings \
+    --no-service-autorun \
+    --user-data-dir="$HOME/.proxybrowser" \
+    --proxy-server="localhost:8080" \
+    --proxy-bypass-list='<-loopback>'
+  '';
+
 in
 {
   options.sconfig.security-tools = lib.mkEnableOption "Enable security tools";
@@ -23,6 +42,7 @@ in
       nmap
       openvpn
       postman
+      proxybrowser
       remmina
       socat
       thc-hydra
