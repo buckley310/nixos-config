efe4bf2d0657074297511bf21d655545ebcdab27
Sean Buckley <sean.bck@gmail.com>
2021-08-27 22:03:45 -0400

    update sway module

--- modules/sway.nix
+++ modules/sway.nix
@@ -6,20 +6,43 @@ in
   options.sconfig.sway = lib.mkEnableOption "Enable Sway Window Manager";
 
   config = lib.mkIf cfg {
-    programs.sway.enable = true;
-    programs.bash.interactiveShellInit = '' [ "$(tty)" = "/dev/tty1" ] && exec sway '';
+    programs.sway = {
+      enable = true;
+      extraSessionCommands = ''
+        export GTK_THEME="Yaru-dark";
+        export MOZ_ENABLE_WAYLAND="1";
+      '';
+    };
 
-    environment.variables.GTK_THEME = "Yaru-dark";
-    environment.variables.MOZ_ENABLE_WAYLAND = "1";
     environment.etc."xdg/gtk-3.0/settings.ini".text = ''
       [Settings]
       gtk-theme-name=Yaru-dark
       gtk-icon-theme-name=Numix
     '';
 
+    environment.etc."sway/config".source = pkgs.runCommand "sway_config" { } ''
+      (
+        cat '${pkgs.sway}/etc/sway/config'
+        cat <<"  EOF"
+          include /etc/sway/config.d/*
+          output DP-1 position    0 0 resolution 2560x1440@165Hz
+          output DP-2 position 2560 0 resolution 2560x1440@165Hz
+        EOF
+      )|
+      sed 's/position top//' |
+      sed 's/status_command while.*/status_command i3status/' |
+      tee "$out"
+    '';
+
     services.gvfs.enable = true;
     programs.dconf.enable = true;
     networking.networkmanager.enable = true;
+
+    services.xserver = {
+      enable = true;
+      libinput.enable = true;
+      displayManager.gdm.enable = true;
+    };
 
     environment.systemPackages = with pkgs; [
       numix-icon-theme
