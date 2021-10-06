f179ff230621af423f84183a1d91b03d88ec2940
Sean Buckley <sean.bck@gmail.com>
2021-10-06 01:00:22 -0400

    make sddm/plasma work with wayland

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -10,6 +10,8 @@ in
       enable = true;
       libinput.enable = true;
       displayManager.sddm.enable = true;
+      displayManager.sddm.settings.Wayland.SessionDir =
+        "${pkgs.plasma5Packages.plasma-workspace}/share/wayland-sessions";
       desktopManager.plasma5.enable = true;
     };
     hardware.pulseaudio.enable = true;
