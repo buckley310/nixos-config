cb4737b15a06fb23d4abb594be3aa1b1e9384d0a
Sean Buckley <sean.bck@gmail.com>
2021-10-10 04:21:36 -0400

    hp: use tmpfs root

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -4,6 +4,23 @@
     openssh.enable = true;
   };
 
+  users.mutableUsers = false;
+  users.users.root.passwordFile = "/nix/persist/shadow_sean";
+  users.users.sean.passwordFile = "/nix/persist/shadow_sean";
+
+  environment.persistence."/nix/persist" = {
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+    ];
+    directories = [
+      "/var/log"
+    ];
+  };
+
   sconfig = {
     gnome = true;
     profile = "desktop";
@@ -21,10 +38,11 @@
   };
 
   fileSystems = {
-    "/" = { device = "zroot/locker/os"; fsType = "zfs"; };
+    "/" = { device = "tmpfs"; fsType = "tmpfs"; options = [ "mode=755" ]; };
+    "/nix" = { device = "zroot/locker/nix"; fsType = "zfs"; };
     "/home" = { device = "zroot/locker/home"; fsType = "zfs"; };
     "/boot" = { device = "/dev/disk/by-partlabel/EFI\\x20system\\x20partition"; fsType = "vfat"; };
   };
 
-  system.stateVersion = "20.09";
+  system.stateVersion = "21.05";
 }
