993429c9464c0c8ef1f5a757e3bef3a391a8b98a
Sean Buckley <sean.bck@gmail.com>
2021-10-28 15:01:50 -0400

    restructure hosts directory

--- hosts/slate/default.nix
+++
@@ -1,4 +0,0 @@
-{
-  system = "x86_64-linux";
-  module = ./configuration.nix;
-}
--- lib/hosts.nix
+++ lib/hosts.nix
@@ -1,20 +1,34 @@
 { path, nixpkgs, nixosModule }:
 let
 
-  hostMetadata = builtins.mapAttrs
-    (name: _: import (path + "/${name}"))
-    (builtins.readDir path);
+  inherit (builtins) mapAttrs attrValues attrNames readDir foldl' listToAttrs;
 
-  getHostConfig = hostName: hostMeta:
-    nixpkgs.lib.nixosSystem
-      {
-        inherit (hostMeta) system;
-        modules = [
-          (nixosModule)
-          (hostMeta.module)
-          (_: { networking.hostName = hostName; })
-        ];
-      };
+  # grab a list of systems, with associated hostnames
+  # { x86_64-linux = [ "host1" "host2" ]; }
+  sysToHosts = mapAttrs
+    (system: _: attrNames (readDir "${path}/${system}"))
+    (readDir path);
+
+  # invert the attrset, from {sys=[name]} to {name=sys}
+  # { host1 = "x86_64-linux"; host2 = "x86_64-linux"; }
+  hostToSys = foldl' (a: b: a // b) { }
+    (attrValues
+      (mapAttrs
+        (system: hosts: listToAttrs (map (name: { inherit name; value = system; }) hosts))
+        sysToHosts));
+
+  # build system configurations
+  # { host1 = <nixosConfiguration>; host2 = <nixosConfiguration>; }
+  hostToConfig = mapAttrs
+    (hostName: system: nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        (nixosModule)
+        ("${path}/${system}/${hostName}")
+        (_: { networking.hostName = hostName; })
+      ];
+    })
+    hostToSys;
 
 in
-builtins.mapAttrs getHostConfig hostMetadata
+hostToConfig
