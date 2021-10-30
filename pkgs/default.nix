de09227bd8fb9439829cf2bcfb80b1d7061c3f63
Sean Buckley <sean.bck@gmail.com>
2021-10-29 23:39:29 -0400

    move some packages to x64-only

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,19 +7,19 @@ let
     {
       commander-x16 = pkgs.callPackage ./commander-x16 { };
       gef = pkgs.callPackage ./gef { };
-      SpaceCadetPinball = pkgs.callPackage ./SpaceCadetPinball { };
       stretchy-spaces = pkgs.callPackage ./stretchy-spaces { };
       webshells = pkgs.callPackage ./webshells { };
       weevely = pkgs.callPackage ./weevely { };
-      security-toolbox = pkgs.callPackage ./security-toolbox {
-        pkgs = pkgs // self.packages.${pkgs.system};
-      };
     };
 
   x64Lin =
     {
       binaryninja = pkgs.callPackage ./binary-ninja-personal { };
       packettracer = pkgs.callPackage ./packettracer { };
+      SpaceCadetPinball = pkgs.callPackage ./SpaceCadetPinball { };
+      security-toolbox = pkgs.callPackage ./security-toolbox {
+        pkgs = pkgs // self.packages.${pkgs.system};
+      };
     };
 
 in
