84ad877aae98f1d5e8fa054a511f81bb819f9f68
Sean Buckley <sean.bck@gmail.com>
2021-11-01 16:02:30 -0400

    fix recursion issue

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,23 +1,17 @@
 pkgs:
-
 let
-
-  allArchs =
-    {
-      commander-x16 = pkgs.callPackage ./commander-x16 { };
-      gef = pkgs.callPackage ./gef { };
-      stretchy-spaces = pkgs.callPackage ./stretchy-spaces { };
-      webshells = pkgs.callPackage ./webshells { };
-      weevely = pkgs.callPackage ./weevely { };
-    };
-
-  x64Lin =
-    {
-      binaryninja = pkgs.callPackage ./binary-ninja-personal { };
-      packettracer = pkgs.callPackage ./packettracer { };
-      security-toolbox = pkgs.callPackage ./security-toolbox { inherit (allArchs) gef webshells weevely; };
-      SpaceCadetPinball = pkgs.callPackage ./SpaceCadetPinball { };
-    };
-
+  empty = pkgs.runCommand "empty" { } "mkdir $out";
+  x64 = x: if pkgs.system == "x86_64-linux" then x else empty;
 in
-allArchs // (if pkgs.system != "x86_64-linux" then { } else x64Lin)
+rec
+{
+  binaryninja = x64 (pkgs.callPackage ./binary-ninja-personal { });
+  commander-x16 = pkgs.callPackage ./commander-x16 { };
+  gef = pkgs.callPackage ./gef { };
+  packettracer = x64 (pkgs.callPackage ./packettracer { });
+  security-toolbox = x64 (pkgs.callPackage ./security-toolbox { inherit gef webshells weevely; });
+  SpaceCadetPinball = x64 (pkgs.callPackage ./SpaceCadetPinball { });
+  stretchy-spaces = pkgs.callPackage ./stretchy-spaces { };
+  webshells = pkgs.callPackage ./webshells { };
+  weevely = pkgs.callPackage ./weevely { };
+}
