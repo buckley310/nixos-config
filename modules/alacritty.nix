c5950a2a7063e7c0df4da785a956274a243feaad
Sean Buckley <sean.bck@gmail.com>
2021-11-02 17:33:58 -0400

    alacritty: make configs work under plasma

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -7,7 +7,22 @@ in
 
   config = lib.mkIf cfg.enable {
 
-    environment.systemPackages = [ pkgs.alacritty ];
+    environment.systemPackages = [
+      pkgs.alacritty
+      (pkgs.writeTextFile {
+        name = "alacritty.yml";
+        destination = "/etc/xdg/alacritty.yml";
+        text = ''
+          env:
+            TERM: xterm-256color
+          key_bindings:
+          - { key: N, mods: Control|Shift, action: SpawnNewInstance }
+          - { key: T, mods: Control|Shift, action: SpawnNewInstance }
+          - { key: PageUp,   mods: Shift, action: ScrollHalfPageUp }
+          - { key: PageDown, mods: Shift, action: ScrollHalfPageDown }
+        '';
+      })
+    ];
 
     programs.bash.interactiveShellInit = ''
       function _set_title() {
@@ -16,15 +31,8 @@ in
       [ -z "$VTE_VERSION" ] && PROMPT_COMMAND="_set_title; $PROMPT_COMMAND"
     '';
 
-    environment.etc."xdg/alacritty.yml".text = ''
-      env:
-        TERM: xterm-256color
-      key_bindings:
-      - { key: N, mods: Control|Shift, action: SpawnNewInstance }
-      - { key: T, mods: Control|Shift, action: SpawnNewInstance }
-      - { key: PageUp,   mods: Shift, action: ScrollHalfPageUp }
-      - { key: PageDown, mods: Shift, action: ScrollHalfPageDown }
-    '';
+    environment.etc."xdg/alacritty.yml".source =
+      "/run/current-system/sw/etc/xdg/alacritty.yml";
 
   };
 }
