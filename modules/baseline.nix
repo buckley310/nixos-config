ec8e701fc225f756277877190c96dbea2ef1f2f9
Sean Buckley <sean.bck@gmail.com>
2021-11-06 21:09:53 -0400

    comments

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -1,13 +1,15 @@
 { config, pkgs, ... }:
 {
   time.timeZone = "US/Eastern";
+
+  # the default is all languages. this just shrinks the install size
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
 
   boot = {
     zfs.forceImportAll = false;
     zfs.forceImportRoot = false;
     kernelParams = [ "amdgpu.gpu_recovery=1" "panic=30" ];
-    initrd.availableKernelModules = [ "nvme" ];
+    initrd.availableKernelModules = [ "nvme" ]; # is this still needed?
   };
 
   nixpkgs.config.allowUnfree = true;
@@ -27,6 +29,8 @@
   zramSwap.enable = true;
 
   networking.hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
+
+  # lower ipv4 DNS priority for networkmanager
   networking.networkmanager.extraConfig = ''
     [connection]
     ipv4.dns-priority=101
