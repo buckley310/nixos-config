f874689eccb17f31f991f502dad8c67a9e46d8dd
Sean Buckley <sean.bck@gmail.com>
2021-11-09 12:20:34 -0500

    move user accounts to separate file

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -21,10 +21,7 @@
     (x: (pkgs.writeShellScriptBin "sc-${x}" "nixos-rebuild ${x} --refresh --flake github:buckley310/nixos-config"))
     [ "switch" "build" "boot" ];
 
-  systemd.tmpfiles.rules = [
-    "e /nix/var/log - - - 30d"
-    "e /home/sean/Downloads - - - 9d"
-  ];
+  systemd.tmpfiles.rules = [ "e /nix/var/log - - - 30d" ];
 
   zramSwap.enable = true;
 
@@ -60,15 +57,5 @@
       publish.enable = true;
       publish.addresses = true;
     };
-  };
-
-  users.users.sean = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "audio" "video" "networkmanager" "dialout" "input" "wireshark" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqWHzIXaF88Y8+64gBlLbZ5ZZcLl08kTHG1clHd7gaq desktop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlMPWSVyDNAvXYtpXCI/geCeUEMbL9Nthm9B0zg1sIy sean@hp"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuuBHq3x28cdQ0JWAZ0R+2rVlRoPnA+MOvpdF5rraGp sean@lenny"
-    ];
   };
 }
---
+++ modules/sean.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+let
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqWHzIXaF88Y8+64gBlLbZ5ZZcLl08kTHG1clHd7gaq desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlMPWSVyDNAvXYtpXCI/geCeUEMbL9Nthm9B0zg1sIy sean@hp"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuuBHq3x28cdQ0JWAZ0R+2rVlRoPnA+MOvpdF5rraGp sean@lenny"
+  ];
+in
+{
+  users.users.root.openssh.authorizedKeys =
+    if config.sconfig.profile == "server"
+    then { inherit keys; } else { };
+
+  users.users.sean = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "audio" "video" "networkmanager" "dialout" "input" "wireshark" ];
+    openssh.authorizedKeys = { inherit keys; };
+  };
+
+  systemd.tmpfiles.rules = [ "e /home/sean/Downloads - - - 9d" ];
+}
