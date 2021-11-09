73ac2a21875eee400f2c659aeb366f4d83108d56
Sean Buckley <sean.bck@gmail.com>
2021-11-09 10:38:54 -0500

    add lenny ssh key

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -68,6 +68,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqWHzIXaF88Y8+64gBlLbZ5ZZcLl08kTHG1clHd7gaq desktop"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlMPWSVyDNAvXYtpXCI/geCeUEMbL9Nthm9B0zg1sIy sean@hp"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuuBHq3x28cdQ0JWAZ0R+2rVlRoPnA+MOvpdF5rraGp sean@lenny"
     ];
   };
 }
