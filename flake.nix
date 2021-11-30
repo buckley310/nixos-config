9194a231e5de29a746d01e68ad7efcaed922176d
Sean Buckley <sean.bck@gmail.com>
2021-11-30 02:24:32 -0500

    21.11-small -> 21.11

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.11-small";
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
   inputs.impermanence.url = "github:nix-community/impermanence";
 
   outputs = { self, nixpkgs, impermanence, ... }:
