12f369fb51ffe62388a07e430f1d6f684f16b783
Sean Buckley <sean.bck@gmail.com>
2021-12-21 14:26:36 -0500

    tidy up hosts/default.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,16 +2,16 @@ nixpkgs: hardware: nixosModule:
 with hardware;
 
 let
-  sys = args: nixpkgs.lib.nixosSystem {
-    system = builtins.head args;
-    modules = [ nixosModule ] ++ builtins.tail args;
+  sys = system: mods: nixpkgs.lib.nixosSystem {
+    inherit system;
+    modules = [ nixosModule ] ++ mods;
   };
 
 in
 {
-  cube = sys [ "x86_64-linux" physical ./cube ];
-  hp = sys [ "x86_64-linux" physical ./hp ];
-  lenny = sys [ "x86_64-linux" physical ./lenny ];
-  nixdev = sys [ "x86_64-linux" qemu ./nixdev ];
-  slate = sys [ "x86_64-linux" physical ./slate ];
+  cube = sys "x86_64-linux" [ physical ./cube ];
+  hp = sys "x86_64-linux" [ physical ./hp ];
+  lenny = sys "x86_64-linux" [ physical ./lenny ];
+  nixdev = sys "x86_64-linux" [ qemu ./nixdev ];
+  slate = sys "x86_64-linux" [ physical ./slate ];
 }
