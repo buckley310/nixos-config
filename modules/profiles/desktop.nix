3747a746cb6939810b26052f4c7e7bbb69ca3472
Sean Buckley <sean.bck@gmail.com>
2021-12-26 23:33:08 -0500

    vscode: remove editor.scrollBeyondLastLine

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -74,7 +74,6 @@ with lib;
 
     environment.etc."vscode-settings.json".text = builtins.toJSON {
       "editor.renderFinalNewline" = false;
-      "editor.scrollBeyondLastLine" = false;
       "extensions.autoCheckUpdates" = false;
       "extensions.autoUpdate" = false;
       "files.insertFinalNewline" = true;
