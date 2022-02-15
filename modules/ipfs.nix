504566121f6171bf23ba0ad17711f3aaccf3f03e
Sean Buckley <sean.bck@gmail.com>
2022-02-15 16:20:40 -0500

    ipfs: add "modes"

--- modules/ipfs.nix
+++ modules/ipfs.nix
@@ -1,17 +1,30 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.sconfig.ipfs;
+
+  modes = {
+    server = {
+      Swarm.DisableNatPortMap = true; # Disable UPnP
+      AutoNAT.ServiceMode = "disabled"; # maybe change this to "enabled" ?
+      Routing.Type = "dhtclient"; # maybe change this to "dht" ?
+      Swarm.ConnMgr.HighWater = 900;
+      Swarm.ConnMgr.LowWater = 600;
+    };
+    desktop = {
+      Swarm.DisableNatPortMap = true; # Disable UPnP
+      AutoNAT.ServiceMode = "disabled";
+      Routing.Type = "dhtclient";
+      Swarm.ConnMgr.HighWater = 300;
+      Swarm.ConnMgr.LowWater = 50;
+    };
+  };
+
 in
 {
   options.sconfig.ipfs = {
 
     enable = lib.mkEnableOption "Turn on IPFS";
-
-    lowpower = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "enable the 'lowpower' profile in IPFS.";
-    };
+    mode = lib.mkOption { type = lib.types.enum [ "server" "desktop" ]; };
 
   };
 
@@ -20,19 +33,15 @@ in
     services.ipfs = {
       enable = true;
       emptyRepo = true;
+      extraConfig = modes.${cfg.mode};
       gatewayAddress = "/ip4/127.0.0.1/tcp/8001";
       # Had issues without fdlimit. 65536 value taken from nixpkgs example
       serviceFdlimit = 65536;
     };
 
-    systemd.services.ipfs = {
-      preStart = lib.mkAfter (lib.optionalString cfg.lowpower ''
-        ipfs --offline config profile apply lowpower
-      '');
-      postStart = ''
-        chmod g+r /var/lib/ipfs/config
-      '';
-    };
+    systemd.services.ipfs.postStart = ''
+      chmod g+r /var/lib/ipfs/config
+    '';
 
   };
 }
