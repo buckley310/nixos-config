6effbf318f503ed5ef84af02da5919ceb07c77fb
Sean Buckley <sean.bck@gmail.com>
2022-05-27 13:13:19 -0400

    remove gef package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,10 +11,9 @@ rec
   bck-nerdfont = pkg ./bck-nerdfont.nix { };
   binaryninja = pkg ./binary-ninja-personal { };
   commander-x16 = pkg ./commander-x16 { };
-  gef = pkg ./gef { };
   mp4grep = pkg ./mp4grep.nix { };
   msfpc = pkg ./msfpc { };
-  security-toolbox = pkg ./security-toolbox { inherit gef msfpc webshells weevely security-wordlists; };
+  security-toolbox = pkg ./security-toolbox { inherit msfpc webshells weevely security-wordlists; };
   security-wordlists = pkg ./wordlists.nix { };
   SpaceCadetPinball = pkg ./SpaceCadetPinball { };
   stretchy-spaces = pkg ./stretchy-spaces { };
