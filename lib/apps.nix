8b45caeafc19aa944c90014411af3b823b4445eb
Sean Buckley <sean.bck@gmail.com>
2022-06-29 14:55:21 -0400

    fix apps

--- lib/apps.nix
+++ lib/apps.nix
@@ -1,14 +1,19 @@
 pkgs:
+builtins.mapAttrs
+  (n: v: {
+    type = "app";
+    program = toString (pkgs.writeShellScript n v);
+  })
 {
   jupyterlab =
     let
       jupy = pkgs.python3.withPackages (p: with p; [ jupyterlab ipython ]);
     in
-    pkgs.writeShellScriptBin "jupyterlab" ''
+    ''
       exec ${jupy}/bin/python -m jupyterlab "$@"
     '';
 
-  qemu-uefi = pkgs.writeShellScriptBin "qemu-uefi" ''
+  qemu-uefi = ''
     exec ${pkgs.qemu_kvm}/bin/qemu-kvm -bios ${pkgs.OVMF.fd}/FV/OVMF.fd "$@"
   '';
 }
