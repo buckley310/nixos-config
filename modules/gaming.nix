66ea336a05b485a4c3439ad447f83078a3992e23
Sean Buckley <sean.bck@gmail.com>
2022-07-07 23:08:04 -0400

    gaming: add switch controller module

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -8,9 +8,17 @@ in
 
   config = lib.mkIf cfg.enable
     {
+
       programs.steam.enable = true;
+
       environment.systemPackages = with pkgs; [
         obs-studio
       ];
+
+      boot.extraModulePackages =
+        lib.optional
+          (lib.versionOlder config.boot.kernelPackages.kernel.version "5.16")
+          (config.boot.kernelPackages.hid-nintendo);
+
     };
 }
