3d10be0f0dad170f5e2ae163f5776c34002fe90c
Sean Buckley <sean.bck@gmail.com>
2022-07-24 12:24:35 -0400

    rm security-toolbox

--- modules/security-tools.nix
+++ modules/security-tools.nix
@@ -1,14 +1,62 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
 
   cfg = config.sconfig.security-tools;
+
+  proxybrowser = pkgs.writeShellScriptBin "proxybrowser" ''
+    exec ${pkgs.chromium}/bin/chromium \
+    --disable-background-networking \
+    --disable-default-apps \
+    --disable-plugins-discovery \
+    --disk-cache-size=0 \
+    --ignore-certificate-errors \
+    --no-default-browser-check \
+    --no-experiments \
+    --no-first-run \
+    --no-pings \
+    --no-service-autorun \
+    --user-data-dir="$HOME/.proxybrowser" \
+    --proxy-server="localhost:8080" \
+    --proxy-bypass-list='<-loopback>'
+  '';
 
 in
 {
   options.sconfig.security-tools = lib.mkEnableOption "Enable security tools";
 
   config = lib.mkIf cfg {
-    environment.systemPackages = [ pkgs.security-toolbox ];
+    environment.systemPackages = with pkgs; [
+      proxybrowser
+
+      binutils
+      bridge-utils
+      burpsuite
+      dhcpdump
+      dirb
+      exiftool
+      feroxbuster
+      ffuf
+      gef
+      ghidra-bin
+      gobuster
+      iptables-nftables-compat
+      macchanger
+      masscan
+      metasploit
+      msfpc
+      net-snmp
+      nmap
+      openvpn
+      postman
+      pwndbg
+      remmina
+      security-wordlists
+      socat
+      thc-hydra
+      webshells
+      weevely
+      wfuzz
+    ];
 
     programs = {
       wireshark.enable = true;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,14 +6,12 @@ let
     in
     if p.meta.available then p else pkgs.emptyDirectory;
 in
-rec
 {
   bck-nerdfont = pkg ./bck-nerdfont.nix { };
   binaryninja = pkg ./binary-ninja-personal { };
   commander-x16 = pkg ./commander-x16 { };
   mp4grep = pkg ./mp4grep.nix { };
   msfpc = pkg ./msfpc { };
-  security-toolbox = pkg ./security-toolbox { inherit msfpc webshells weevely security-wordlists; };
   security-wordlists = pkg ./wordlists.nix { };
   SpaceCadetPinball = pkg ./SpaceCadetPinball { };
   stretchy-spaces = pkg ./stretchy-spaces { };
