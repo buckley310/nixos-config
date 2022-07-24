afe3fd1179b6cc9dc18fa52e6e07169dd816dd7f
Sean Buckley <sean.bck@gmail.com>
2022-07-24 12:18:18 -0400

    wordlists: maintenance

--- pkgs/wordlists.nix
+++ pkgs/wordlists.nix
@@ -7,27 +7,12 @@
 }:
 let
 
-  nmap-data = runCommand "nmap-data" { } ''
-    tar --strip-components=1 -xvf ${nmap.src}
-    mv nselib/data $out
-  '';
-
   seclists = fetchFromGitHub {
     owner = "danielmiessler";
     repo = "SecLists";
-    rev = "cb81804316c634728bbddb857ce7dfa5016e01b1";
-    sha256 = "QBlZlS8JJI6pIdIaD1I+7gMuPPfEybxybj2HrnQM7co=";
+    rev = "2022.2";
+    sha256 = "Xi4FsrVYioR5HBVJzo0bug5ETgV2mCtulPOpBXm+Y1s=";
   };
-
-  rockyou = runCommand "rockyou.txt"
-    {
-      outputHashAlgo = "sha256";
-      outputHash = "0wv1d2b00x294irnqki9vvbicmysdsa1vphkqmhhbjs2fzm5y0qn";
-    }
-    ''
-      url="https://gitlab.com/kalilinux/packages/wordlists/-/raw/upstream/0.3/rockyou.txt.gz"
-      ${curl}/bin/curl --insecure "$url" | gunzip >$out
-    '';
 
   dirbuster = runCommand "dirbuster"
     {
@@ -46,8 +31,7 @@ in
 runCommand "wordlists" { } ''
   mkdir -p $out/share/wordlists
   ln -s ${wfuzz.src}/wordlist $out/share/wordlists/wfuzz
-  ln -s ${nmap-data} $out/share/wordlists/nmap
+  ln -s ${nmap}/share/nmap/nselib/data $out/share/wordlists/nmap
   ln -s ${seclists} $out/share/wordlists/seclists
-  ln -s ${rockyou} $out/share/wordlists/rockyou.txt
   ln -s ${dirbuster} $out/share/wordlists/dirbuster
 ''
