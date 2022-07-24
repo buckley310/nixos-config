d67871c1068dbfb6ddac90596a2a8b84401b95f7
Sean Buckley <sean.bck@gmail.com>
2022-07-24 11:07:14 -0400

    binary-ninja-personal: cleanup

--- pkgs/binary-ninja-personal/default.nix
+++ pkgs/binary-ninja-personal/default.nix
@@ -1,51 +1,20 @@
 { stdenv
-, alsaLib
 , autoPatchelfHook
-, dbus
-, fontconfig
-, freetype
-, glib
-, krb5
-, libglvnd
-, libICE
-, libSM
-, libX11
-, libXcomposite
-, libXcursor
-, libXdamage
-, libXext
-, libXi
-, libxkbcommon
-, libXrandr
-, libXrender
-, libXtst
 , makeWrapper
 , ncurses
-, nss
+, qt6
 , python3
 , requireFile
-, systemd
 , unzip
-, xcbutilimage
-, xcbutilkeysyms
-, xcbutilrenderutil
-, xcbutilwm
-, xkeyboardconfig
-, zlib
 }:
 
 let
   hjs = builtins.fromJSON (builtins.readFile ./hashes.js);
 
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "binaryninja";
   inherit (hjs) version;
-
-  autoPatchelfIgnoreMissingDeps = [
-    "libQt6PrintSupport.so.6"
-    "libQt6Qml.so.6"
-  ];
 
   src = requireFile rec {
     name = "BinaryNinja-personal.zip";
@@ -53,51 +22,22 @@ stdenv.mkDerivation rec {
     sha256 = hjs.hashes.${name};
   };
 
-  buildInputs = [
-    alsaLib
+  nativeBuildInputs = [
     autoPatchelfHook
-    dbus
-    fontconfig
-    freetype
-    glib
-    krb5
-    libglvnd
-    libICE
-    libSM
-    libX11
-    libXcomposite
-    libXcursor
-    libXdamage
-    libXext
-    libXi
-    libxkbcommon
-    libXrandr
-    libXrender
-    libXtst
     makeWrapper
-    ncurses
-    nss
-    python3
-    stdenv.cc.cc.lib
+    qt6.wrapQtAppsHook
     unzip
-    xcbutilimage
-    xcbutilkeysyms
-    xcbutilrenderutil
-    xcbutilwm
-    zlib
   ];
 
-  dontStrip = true;
-  dontPatchELF = true;
+  buildInputs = [
+    ncurses
+  ];
 
   installPhase = ''
-    mkdir -p $out/lib $out/bin $out/share
-    mv $NIX_BUILD_TOP/$sourceRoot $out/lib/binary-ninja
-    makeWrapper $out/lib/binary-ninja/binaryninja $out/bin/binaryninja \
-        --suffix LD_LIBRARY_PATH : "${systemd}/lib" \
-        --suffix LD_LIBRARY_PATH : "${python3}/lib" \
-        --set QT_XKB_CONFIG_ROOT "${xkeyboardconfig}/share/X11/xkb" \
-        --set QTCOMPOSE "${libX11.out}/share/X11/locale"
+    mkdir -p $out/lib $out/bin
+    cp -a . $out/lib/binaryninja-personal
+    makeWrapper $out/lib/binaryninja-personal/binaryninja $out/bin/binaryninja \
+        --suffix LD_LIBRARY_PATH : "${python3}/lib"
   '';
 
   meta.platforms = [ "x86_64-linux" ];
