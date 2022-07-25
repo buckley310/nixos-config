4cf06fce353943997535a882866baa3c4ca87f7d
Sean Buckley <sean.bck@gmail.com>
2022-07-24 22:11:25 -0400

    hp: move persistence

--- hosts/hp/default.nix
+++ hosts/hp/default.nix
@@ -1,29 +1,27 @@
 { config, lib, pkgs, ... }:
 {
-  services = {
-    openssh.enable = true;
-  };
-
-  users.mutableUsers = false;
-  users.users.root.passwordFile = "/nix/persist/shadow_sean";
-  users.users.sean.passwordFile = "/nix/persist/shadow_sean";
-
-  environment.etc =
-    lib.genAttrs
-      [
-        "machine-id"
-        "NetworkManager/system-connections"
-        "ssh/ssh_host_ed25519_key"
-        "ssh/ssh_host_rsa_key"
-      ]
-      (name: { source = "/nix/persist/etc/${name}"; });
-
   sconfig = {
     gnome = true;
     profile = "desktop";
     horizon.enable = true;
     wg-home.enable = true;
   };
+
+  environment.etc.machine-id.source = "/var/lib/nixos/machine-id";
+
+  environment.etc."NetworkManager/system-connections".source =
+    "/var/lib/nixos/nm-connections";
+
+  services.openssh = {
+    enable = true;
+    hostKeys = [
+      { type = "ed25519"; path = "/var/lib/nixos/ssh_host_ed25519_key"; }
+    ];
+  };
+
+  users.mutableUsers = false;
+  users.users.sean.passwordFile = "/var/lib/nixos/shadow_sean";
+  users.users.root.passwordFile = "/var/lib/nixos/shadow_sean";
 
   boot = {
     loader.systemd-boot.enable = true;
@@ -35,10 +33,11 @@
 
   fileSystems = {
     "/" = { device = "tmpfs"; fsType = "tmpfs"; options = [ "mode=755" ]; };
-    "/nix" = { device = "zroot/locker/nix"; fsType = "zfs"; };
-    "/home" = { device = "zroot/locker/home"; fsType = "zfs"; };
     "/boot" = { device = "/dev/disk/by-partlabel/EFI\\x20system\\x20partition"; fsType = "vfat"; };
-    "/var/log" = { device = "/nix/persist/var/log"; noCheck = true; options = [ "bind" ]; };
+    "/home" = { device = "zroot/locker/home"; fsType = "zfs"; };
+    "/nix" = { device = "zroot/locker/nix"; fsType = "zfs"; };
+    "/var/lib" = { device = "zroot/locker/lib"; fsType = "zfs"; };
+    "/var/log" = { device = "zroot/locker/log"; fsType = "zfs"; };
   };
 
   system.stateVersion = "21.05";
