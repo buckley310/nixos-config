6e1816dc6b192d98db4f7723bcfa1dfd22323b11
Sean Buckley <sean.bck@gmail.com>
2022-11-04 01:39:12 -0400

    levi: update kube.nix for Calico

--- hosts/levi/kube.nix
+++ hosts/levi/kube.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 {
   networking.firewall.allowedTCPPorts = [ 6443 ];
-  environment.systemPackages = [ pkgs.kubectl ];
+  environment.systemPackages = [
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+  ];
   services.k3s.enable = true;
+  services.k3s.extraFlags = toString [
+    # flags for using Calico instead of Flannel
+    "--cluster-cidr=192.168.0.0/16"
+    "--disable-network-policy"
+    "--flannel-backend=none"
+  ];
 }
