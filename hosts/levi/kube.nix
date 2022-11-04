c581d65442879cf0e72e508254076bb337d97ee8
Sean Buckley <sean.bck@gmail.com>
2022-11-04 12:04:27 -0400

    levi: switch kube network

--- hosts/levi/kube.nix
+++ hosts/levi/kube.nix
@@ -8,7 +8,6 @@
   services.k3s.enable = true;
   services.k3s.extraFlags = toString [
     # flags for using Calico instead of Flannel
-    "--cluster-cidr=192.168.0.0/16"
     "--disable-network-policy"
     "--flannel-backend=none"
   ];
