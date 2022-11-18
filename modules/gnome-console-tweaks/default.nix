02416e30d393f93e3330a2fd365adada2ba8206f
Sean Buckley <sean.bck@gmail.com>
2022-11-17 22:26:44 -0500

    gnome-console: dont prompt when closing a tab

--- modules/gnome-console-tweaks/default.nix
+++ modules/gnome-console-tweaks/default.nix
@@ -4,6 +4,7 @@
       gnome-console = super.gnome-console.overrideAttrs (prev: {
         patches = prev.patches ++ [
           ./background.patch
+          ./no-close-tab-prompt.patch
           ./tango.patch
         ];
       });
