c56f83fbadd17678f2536f79756c8bff2d7c16f1
Sean Buckley <sean.bck@gmail.com>
2022-12-07 19:09:49 -0500

    revert unstable zfs

--- hosts/levi/optimus.nix
+++ hosts/levi/optimus.nix
@@ -6,7 +6,6 @@ let
     hardware.nvidia.prime.intelBusId = "PCI:0:2:0";
     hardware.nvidia.prime.nvidiaBusId = "PCI:1:0:0";
     boot.kernelPackages = pkgs.linuxPackages_6_0;
-    boot.zfs.enableUnstable = true; # TODO: remove
   };
 
   available-modes = {
