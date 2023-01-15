849e957bb7295da0158364fc93900c60422ea854
Sean Buckley <sean.bck@gmail.com>
2023-01-14 21:31:42 -0500

    add chromium policy

---
+++ modules/chromium.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+{
+  nixpkgs.config.chromium.commandLineArgs = toString [
+    "--enable-features=WebUIDarkMode"
+    "--force-dark-mode"
+  ];
+  programs.chromium = {
+    enable = true;
+    extraOpts = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      BlockThirdPartyCookies = true;
+      BrowserAddPersonEnabled = false;
+      BrowserGuestModeEnabled = false;
+      DefaultGeolocationSetting = 2;
+      DefaultNotificationsSetting = 2;
+      PasswordManagerEnabled = false;
+      SyncDisabled = true; # required for BrowsingDataLifetime
+      BrowsingDataLifetime = [
+        { data_types = [ "browsing_history" ]; time_to_live_in_hours = 24 * 7; }
+        { data_types = [ "download_history" ]; time_to_live_in_hours = 6; }
+      ];
+    };
+  };
+}
