b62f9d49bcc60467016295d88f057c1e1d02aa86
Sean Buckley <sean.bck@gmail.com>
2023-01-15 19:14:25 -0500

    security-tools: remove proxybrowser

--- modules/security-tools.nix
+++ modules/security-tools.nix
@@ -3,31 +3,12 @@ let
 
   cfg = config.sconfig.security-tools;
 
-  proxybrowser = pkgs.writeShellScriptBin "proxybrowser" ''
-    exec ${pkgs.chromium}/bin/chromium \
-    --disable-background-networking \
-    --disable-default-apps \
-    --disable-plugins-discovery \
-    --disk-cache-size=0 \
-    --ignore-certificate-errors \
-    --no-default-browser-check \
-    --no-experiments \
-    --no-first-run \
-    --no-pings \
-    --no-service-autorun \
-    --user-data-dir="$HOME/.proxybrowser" \
-    --proxy-server="localhost:8080" \
-    --proxy-bypass-list='<-loopback>'
-  '';
-
 in
 {
   options.sconfig.security-tools = lib.mkEnableOption "Enable security tools";
 
   config = lib.mkIf cfg {
     environment.systemPackages = with pkgs; [
-      proxybrowser
-
       binutils
       bridge-utils
       burpsuite
