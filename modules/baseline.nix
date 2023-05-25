bbcd77fe00889f34eb01a0fc49ff6bcc0cd440c2
Sean Buckley <sean.bck@gmail.com>
2023-05-25 13:16:25 -0400

    comments

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -10,7 +10,7 @@
 
   boot = {
     zfs.forceImportRoot = false;
-    initrd.availableKernelModules = [ "nvme" ]; # is this still needed?
+    initrd.availableKernelModules = [ "nvme" ];
     kernelParams = [
       "amdgpu.gpu_recovery=1"
       "nohibernate"
