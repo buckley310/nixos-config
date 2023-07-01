d0a2ff241f027e61790e4335677b8cd94cbd279a
Sean Buckley <sean.bck@gmail.com>
2023-06-30 22:25:25 -0400

    increase inotify limits

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -11,6 +11,10 @@
   boot = {
     zfs.forceImportRoot = false;
     initrd.availableKernelModules = [ "nvme" ];
+    kernel.sysctl = {
+      "fs.inotify.max_user_instances" = 65536;
+      "fs.inotify.max_user_watches" = 1048576;
+    };
     kernelParams = [
       "amdgpu.gpu_recovery=1"
       "nohibernate"
