2a25f96d615387db5e03f08ca4c01ed29258b485
Sean Buckley <sean.bck@gmail.com>
2023-07-09 21:20:04 -0400

    optimize gnome-console-tweaks logic

--- modules/gnome-console-tweaks/default.nix
+++ modules/gnome-console-tweaks/default.nix
@@ -1,18 +1,15 @@
 {
   nixpkgs.overlays = [
     (self: super: {
-      gnome-console = super.gnome-console.overrideAttrs (prev: {
-        patches =
-          (
-            if (builtins.elem "patches" (builtins.attrNames prev))
-            then prev.patches
-            else [ ]
-          ) ++ [
+      gnome-console = super.gnome-console.overrideAttrs (
+        { patches ? [ ], ... }: {
+          patches = patches ++ [
             ./no-notification.patch
             ./no-warn-close.patch
             ./tango.patch
           ];
-      });
+        }
+      );
     })
   ];
 }
