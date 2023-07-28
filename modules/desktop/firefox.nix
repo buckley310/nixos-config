bb64924ea917990c96bd50c2570459d40e92607f
Sean Buckley <sean.bck@gmail.com>
2023-07-28 00:30:00 -0400

    manage extensions by policy

--- modules/desktop/firefox.nix
+++ modules/desktop/firefox.nix
@@ -22,6 +22,17 @@
             "network.IDN_show_punycode" = true;
             "ui.key.menuAccessKeyFocuses" = false;
           };
+        ExtensionSettings =
+          { "*".installation_mode = "blocked"; } //
+          builtins.mapAttrs
+            (n: v: {
+              installation_mode = "force_installed";
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/${v}/latest.xpi";
+            })
+            {
+              "jid1-KKzOGWgsW3Ao4Q@jetpack" = "i-dont-care-about-cookies";
+              "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
+            };
       };
     })
   ];
