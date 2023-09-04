3976ab128291b3fe6454177845d9557047c78c65
Sean Buckley <sean.bck@gmail.com>
2023-09-03 21:54:22 -0400

    remove "sc-" aliases

--- modules/sean.nix
+++ modules/sean.nix
@@ -17,20 +17,6 @@
 
   systemd.tmpfiles.rules = [ "e /home/sean/Downloads - - - 9d" ];
 
-  environment.systemPackages =
-    [
-      (pkgs.writeShellScriptBin "sc-check" ''
-        out="$(mktemp -d)"
-        nix build -o "$out/out" \
-          "bck#nixosConfigurations.$(hostname).config.system.build.toplevel"
-        readlink /run/current-system "$out/out"
-        rm -r "$out"
-      '')
-    ]
-    ++ map
-      (x: (pkgs.writeShellScriptBin "sc-${x}" "nixos-rebuild ${x} --refresh --flake bck"))
-      [ "boot" "build" "switch" "test" ];
-
   environment.etc."my-settings.sh".text = ''
     git config --global user.email "sean.bck@gmail.com"
     git config --global user.name "Sean Buckley"
