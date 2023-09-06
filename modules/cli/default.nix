fc6d55bb469ff492ab8dd2b413280ceaf5b0b457
Sean Buckley <sean.bck@gmail.com>
2023-09-05 22:35:03 -0400

    move vim  to new file

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -1,8 +1,4 @@
-{ config, pkgs, lib, ... }:
-let
-  pl = lib.importJSON ./powerline-chars.json;
-
-in
+{ config, lib, pkgs, ... }:
 {
   imports = [
     ./kubernetes.nix
@@ -147,29 +143,4 @@ in
       "nix-prefetch-github"
       "nix-shell"
     ];
-
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    defaultEditor = true;
-    configure = {
-      packages.sconfig.start = with pkgs.vimPlugins; [
-        vim-gitgutter
-        vim-nix
-      ];
-      customRC = ''
-        set mouse=
-        set encoding=utf-8
-        scriptencoding utf-8
-        set list nowrap scrolloff=9 updatetime=300 number
-        highlight GitGutterAdd    ctermfg=10
-        highlight GitGutterChange ctermfg=11
-        highlight GitGutterDelete ctermfg=9
-        let g:gitgutter_sign_removed = '${pl.lower_left_triangle}'
-        let g:gitgutter_sign_removed_first_line = '${pl.upper_left_triangle}'
-        let g:gitgutter_sign_modified_removed = '~~'
-      '';
-    };
-  };
 }
