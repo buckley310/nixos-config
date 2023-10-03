c5c8719dc7271b34c3a7f2f49d9d198490a73f96
Sean Buckley <sean.bck@gmail.com>
2023-10-03 12:27:34 -0400

    add lunarvim

--- modules/vim/default.nix
+++ modules/vim/default.nix
@@ -1,45 +1,28 @@
 { config, lib, pkgs, ... }:
 {
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    defaultEditor = true;
-    configure = {
-      packages.sconfig.start = with pkgs.vimPlugins; [
-        airline
-        bufferline-nvim
-        coc-nvim
-        coc-tsserver
-        nerdtree
-        nerdtree-git-plugin
-        vim-autoformat
-        vim-code-dark
-        vim-devicons
-        vim-gitgutter
-        vim-nix
-        vim-startify
-        vim-terraform
-      ] ++
-      # skip syntax-highlight on nixos 23.05
-      lib.optional
-        (vim-nerdtree-syntax-highlight.version != "2021-01-11")
-        (vim-nerdtree-syntax-highlight);
+  environment.variables.EDITOR = "vim";
 
-      customRC = ''
-        source ${./init.vim}
-        luafile ${./init.lua}
-        if filereadable(expand("~/.config/nvim/init.vim"))
-          source ~/.config/nvim/init.vim
-        endif
-      '';
-    };
-  };
+  environment.etc."lvim.lua".source = ./init.lua;
+
+  environment.etc."my-settings.sh".text = ''
+    mkdir -p ~/.config/lvim
+    echo 'vim.cmd("luafile /etc/lvim.lua")' >~/.config/lvim/config.lua
+  '';
 
   environment.systemPackages = with pkgs; [
+    lunarvim
     nodePackages.prettier
+
     (writeShellScriptBin "black" ''
       exec ${pkgs.python3.pkgs.black}/bin/black "$@"
+    '')
+
+    (writeShellScriptBin "vi" ''
+      exec lvim "$@"
+    '')
+
+    (writeShellScriptBin "vim" ''
+      exec lvim "$@"
     '')
   ];
 }
