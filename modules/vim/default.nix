5df3516aa89fcbe176b0443ce81ec34279a6f8ae
Sean Buckley <sean.bck@gmail.com>
2023-10-15 17:13:47 -0400

    switch lvim to new PR

--- modules/vim/default.nix
+++ modules/vim/default.nix
@@ -2,27 +2,12 @@
 {
   environment.variables.EDITOR = "vim";
 
-  environment.etc."lvim.lua".source = ./init.lua;
-
-  environment.etc."my-settings.sh".text = ''
-    mkdir -p ~/.config/lvim
-    echo 'vim.cmd("luafile /etc/lvim.lua")' >~/.config/lvim/config.lua
-  '';
-
   environment.systemPackages = with pkgs; [
     lunarvim
     nodePackages.prettier
 
     (writeShellScriptBin "black" ''
       exec ${pkgs.python3.pkgs.black}/bin/black "$@"
-    '')
-
-    (writeShellScriptBin "vi" ''
-      exec lvim "$@"
-    '')
-
-    (writeShellScriptBin "vim" ''
-      exec lvim "$@"
     '')
   ];
 }
