01526cfc34dbc3b244cea3380f235fb116f8d4b0
Sean Buckley <sean.bck@gmail.com>
2023-10-16 11:00:18 -0400

    fix lunarvim first-run issue

--- modules/vim/init.lua
+++ modules/vim/init.lua
@@ -4,20 +4,22 @@ vim.opt.whichwrap = ""
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pylyzer" })
 
-lvim.format_on_save = true
-
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-	{ exe = "nixpkgs-fmt", filetypes = { "nix" } },
-	{ exe = "black", filetypes = { "python" } },
-	{
-		name = "prettier",
-		filetypes = {
-			"html",
-			"json",
-			"js",
-			"typescript",
-			"yaml"
+-- null-ls is missing on first-run. only do this if null-ls exists
+if pcall(require, "null-ls") then
+	lvim.format_on_save = true
+	local formatters = require "lvim.lsp.null-ls.formatters"
+	formatters.setup {
+		{ exe = "nixpkgs-fmt", filetypes = { "nix" } },
+		{ exe = "black", filetypes = { "python" } },
+		{
+			name = "prettier",
+			filetypes = {
+				"html",
+				"json",
+				"js",
+				"typescript",
+				"yaml"
+			},
 		},
-	},
-}
+	}
+end
