9809e240bb6f1dd705f7f9ff61bba13e745595bd
Sean Buckley <sean.bck@gmail.com>
2023-11-27 22:54:41 -0500

    deploy: add nixos-rebuild to PATH

--- pkgs/deploy/default.nix
+++ pkgs/deploy/default.nix
@@ -1,5 +1,9 @@
-{ writeShellScriptBin, python3 }:
+{ nixos-rebuild
+, python3
+, writeShellScriptBin
+}:
 
 writeShellScriptBin "deploy" ''
+  export PATH="$PATH:${nixos-rebuild}/bin"
   exec ${python3}/bin/python ${./deploy.py} "$@"
 ''
