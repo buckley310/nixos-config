b67867f0e412ed2e41cee8143dce4cd14c8a47c2
Sean Buckley <sean.bck@gmail.com>
2024-03-02 01:05:29 -0500

    binaryninja: 3.5.4526 -> 4.0.4911

--- pkgs/binaryninja/update.sh
+++ pkgs/binaryninja/update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+set -eu
 
 cd "$(dirname "$BASH_SOURCE")"
 
-curl https://binary.ninja/js/hashes.js -o hashes.json
+curl https://binary.ninja/js/hashes.js -o tmp.json
+jq --tab --sort-keys <tmp.json >hashes.json
+rm tmp.json
