2f5cf077225a31ae308fbe548bccd67a81458091
Sean Buckley <sean.bck@gmail.com>
2024-05-26 22:19:44 -0400

    update levi wayland config

--- hosts/levi/wayland.nix
+++ hosts/levi/wayland.nix
@@ -1,7 +1,14 @@
+{ config, ... }:
 {
   # NVIDIA-only using MUX switch in BIOS.
   environment.variables.MUTTER_DEBUG_FORCE_KMS_MODE = "simple";
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.open = false;
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Nvidia driver 555 is significant for wayland support.
+  # Currently version 555 is beta.
+  hardware.nvidia.package = builtins.trace
+    "Using beta nvidia driver"
+    config.boot.kernelPackages.nvidiaPackages.beta;
 }
