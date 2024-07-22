dd406792a00628ea48e7dd877b8ba4cd424e9d29
Sean Buckley <sean.bck@gmail.com>
2024-07-22 02:58:21 -0400

    binaryninja: disable auto updates

--- pkgs/binaryninja/default.nix
+++ pkgs/binaryninja/default.nix
@@ -36,7 +36,9 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/lib $out/bin
     cp -a . $out/lib/binaryninja-personal
+    uppath="~/.binaryninja/update/`echo -n $out/lib/binaryninja-personal|sha256sum|cut -c-64`"
     makeWrapper $out/lib/binaryninja-personal/binaryninja $out/bin/binaryninja \
+        --run "install -D ${builtins.toFile "noauto" "{\"auto\":false}"} $uppath/manifest" \
         --suffix LD_LIBRARY_PATH : "${python3}/lib"
   '';
 
