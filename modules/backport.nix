7b58e3531a0bdbff5df08a10b4c0a3de735a8ca8
Sean Buckley <sean.bck@gmail.com>
2024-09-08 18:41:36 -0400

    add brave backport script

---
+++ modules/backport.nix
@@ -0,0 +1,28 @@
+let
+
+  brave = (final: prev: {
+    brave =
+      let
+        # updates to the newer version before it hits the channels
+        version = "1";
+        hash = "";
+      in
+      if
+        prev.lib.versionAtLeast prev.brave.version version
+      then
+        prev.brave
+      else
+        prev.brave.overrideAttrs {
+          src = prev.fetchurl {
+            inherit hash;
+            url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
+          };
+        };
+  });
+
+in
+{
+  nixpkgs.overlays = [
+    brave
+  ];
+}
