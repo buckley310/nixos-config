e618ba481fe7377dd22e2c5cf94cc19c3d5b4724
Sean Buckley <sean.bck@gmail.com>
2024-10-26 02:30:38 -0400

    merge kubernetes.nix into devtools.nix

--- modules/cli/kubernetes.nix
+++ modules/cli/kubernetes.nix
@@ -1,18 +1,6 @@
 { pkgs, ... }:
 {
-  programs.bash.interactiveShellInit = ''
-    source <(kubectl completion bash)
-    complete -F __start_kubectl k
-  '';
   environment.systemPackages = with pkgs; [
-    kubectl
-    kubernetes-helm
-    stern
-
-    (google-cloud-sdk.withExtraComponents
-      [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
-
     # dedicated script, because bash aliases dont work with `watch`
-    (writeShellScriptBin "k" "exec kubectl \"$@\"")
   ];
 }
--- modules/devtools.nix
+++ modules/devtools.nix
@@ -14,6 +14,8 @@ in
         errcheck
         go
         gopls
+        kubectl
+        kubernetes-helm
         lua-language-server
         nil
         nodePackages.prettier
@@ -23,8 +25,19 @@ in
         rustc
         rustc.llvmPackages.lld
         rustfmt
+        stern
         vscode-langservers-extracted
         yaml-language-server
+
+        # dedicated script, because bash aliases dont work with `watch`
+        (writeShellScriptBin "k" "exec kubectl \"$@\"")
+
+        (google-cloud-sdk.withExtraComponents
+          [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
       ];
+    programs.bash.interactiveShellInit = ''
+      source <(kubectl completion bash)
+      complete -F __start_kubectl k
+    '';
   };
 }
