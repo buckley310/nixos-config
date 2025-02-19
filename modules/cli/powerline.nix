e40cc1c44537197a94ca885ec3657a80a98fb878
Sean Buckley <sean.bck@gmail.com>
2025-02-19 18:14:08 -0500

    simplify powerline config

--- modules/cli/powerline.nix
+++ modules/cli/powerline.nix
@@ -1,33 +1,17 @@
 { pkgs, ... }:
 let
-  left_hard_divider = builtins.fromJSON ''"\uE0B0" '';
-  upper_left_triangle = builtins.fromJSON ''"\uE0BC" '';
-
   theme = {
     BoldForeground = true;
     CwdFg = 231;
+    CmdPassedFg = 231;
+    CmdPassedBg = 102;
     PathBg = 24;
     PathFg = 231;
     SeparatorFg = 16;
   };
 
-  customEnd = [
-    {
-      Content = "$";
-      Foreground = 231;
-      Background = 102;
-      Separator = left_hard_divider;
-    }
-  ];
-
-  plconfig = builtins.toFile "powerline-config.json" (
-    builtins.toJSON {
-      modes.patched.Separator = upper_left_triangle;
-    }
-  );
-
   args = [
-    "-modules=\${remote:+'user,host,'}nix-shell,git,jobs,cwd,newline,customend"
+    "-modules=\${remote:+'user,host,'}nix-shell,git,jobs,cwd,newline,root"
     "-git-assume-unchanged-size 0"
     "-theme /etc/powerline-theme.json"
     "-path-aliases '~/git=~/git'"
@@ -40,14 +24,11 @@ in
 {
   environment.systemPackages = [
     pkgs.powerline-go
-
-    (pkgs.writeShellScriptBin "powerline-go-customend" "echo '${builtins.toJSON customEnd}'")
   ];
 
   environment.etc."powerline-theme.json".text = builtins.toJSON theme;
 
   programs.bash.interactiveShellInit = ''
-    install -D ${plconfig} ~/.config/powerline-go/config.json
     function _update_ps1() {
       local remote=y
       [ "$XDG_SESSION_TYPE" = "x11" ] && unset remote
