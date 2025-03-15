864b4439d4dbdfcac112e1b71fbde442be4e0e98
Sean Buckley <sean.bck@gmail.com>
2025-03-14 20:08:35 -0400

    levI; use latest nvidia

--- hosts/levi/xorg.nix
+++ hosts/levi/xorg.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   ### NVIDIA-only using MUX switch in BIOS.
   services.xserver.displayManager.gdm.wayland = false;
@@ -5,6 +6,8 @@
   services.xserver.screenSection = ''
     Option "metamodes" "DP-2: 2560x1440_165 +0+0 {AllowGSYNCCompatible=On}"
   '';
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
 
   sconfig.gnome = true;
 }
