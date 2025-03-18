f7c3ab06e64f884f7174391608a2afaff604be88
Sean Buckley <sean.bck@gmail.com>
2025-03-18 00:59:12 -0400

    move hyprland config

---
+++ modules/hyprland.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.sconfig.hypr;
+in
+{
+
+  options.sconfig.hypr.enable = lib.mkEnableOption "Hyprland Desktop";
+
+  config = lib.mkIf cfg.enable {
+    hardware.graphics.enable = true;
+    programs.hyprland.enable = true;
+    programs.hyprland.withUWSM = true;
+
+    environment.extraInit = lib.mkAfter ''
+      uwsm check may-start && exec uwsm start hyprland-uwsm.desktop
+    '';
+
+    environment.systemPackages = with pkgs; [
+      hyprlock
+      nautilus
+      playerctl
+      swayidle
+      vimix-cursors
+      waybar
+      wofi
+    ];
+  };
+}
