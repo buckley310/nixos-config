658b9ceb906fa9cb0681c21275820d968ef7b330
Sean Buckley <sean.bck@gmail.com>
2025-04-07 02:19:18 -0400

    overhaul tmux hotkeys

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -113,8 +113,15 @@
     terminal = "screen-256color";
     extraConfig = ''
       bind-key j command-prompt -p "Join pane:"  "join-pane -s '%%'"
-      bind-key '"' split-window -v -c "#{pane_current_path}"
-      bind-key '%' split-window -h -c "#{pane_current_path}"
+      bind-key '_' split-window -v -c "#{pane_current_path}"
+      bind-key '|' split-window -h -c "#{pane_current_path}"
+      bind-key -n C-n new-window   -c "#{pane_current_path}"
+      bind-key -n C-j previous-window
+      bind-key -n C-k next-window
+      bind-key -n C-h select-pane -t -1
+      bind-key -n C-l select-pane -t +1
+      bind-key -n S-Pagedown copy-mode \; send-keys -X halfpage-down
+      bind-key -n S-Pageup   copy-mode \; send-keys -X halfpage-up
       set -g base-index 1
       set -g history-limit 10000
       set -g pane-base-index 1
@@ -123,25 +130,6 @@
       set -sa terminal-overrides ",*256color:Tc"
       # escape-time reduces the time where the escape key acts as an alt key
       set -s escape-time 10
-      ################################################################################
-      # Smart pane switching with awareness of Vim splits.
-      # See: https://github.com/christoomey/vim-tmux-navigator
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
-      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
-      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
-      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
-      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-        "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-        "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
-      bind-key -T copy-mode-vi 'C-h' select-pane -L
-      bind-key -T copy-mode-vi 'C-j' select-pane -D
-      bind-key -T copy-mode-vi 'C-k' select-pane -U
-      bind-key -T copy-mode-vi 'C-l' select-pane -R
-      bind-key -T copy-mode-vi 'C-\' select-pane -l
     '';
   };
 
