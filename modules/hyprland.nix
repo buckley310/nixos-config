40bf057c4b0c8ab895f89c07dc3ea75be897b555
Sean Buckley <sean.bck@gmail.com>
2025-04-27 22:23:57 -0400

    enable bluetooth on wayland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -12,9 +12,11 @@ in
   options.sconfig.hypr.enable = lib.mkEnableOption "Hyprland Desktop";
 
   config = lib.mkIf cfg.enable {
+    hardware.bluetooth.enable = true;
     hardware.graphics.enable = true;
     programs.hyprland.enable = true;
     programs.hyprland.withUWSM = true;
+    services.blueman.enable = true;
 
     environment.extraInit = lib.mkAfter ''
       uwsm check may-start && exec uwsm start hyprland-uwsm.desktop
