134648d617d8fcd89c78c00482d1a5ae92dba323
Sean Buckley <sean.bck@gmail.com>
2025-08-27 01:45:25 -0400

    fix wxedid schemas

--- pkgs/wxedid.nix
+++ pkgs/wxedid.nix
@@ -1,6 +1,8 @@
 {
   bash,
   fetchurl,
+  gtk3,
+  makeWrapper,
   stdenv,
   wxGTK32,
 }:
@@ -8,6 +10,7 @@ stdenv.mkDerivation rec {
   pname = "wxedid";
   version = "0.0.32";
   buildInputs = [ wxGTK32 ];
+  nativeBuildInputs = [ makeWrapper ];
   prePatch = ''
     sed -i "s,/bin/bash,${bash}/bin/bash," "./src/rcode/rcd_autogen"
   '';
@@ -15,4 +18,9 @@ stdenv.mkDerivation rec {
     url = "https://downloads.sourceforge.net/${pname}/${pname}-${version}.tar.gz";
     sha256 = "5d86d136e43e85ad3980982cecff87cf175192c151247c4b8960df9c5e461cc2";
   };
+  postInstall = ''
+    mv $out/bin/wxedid $out/bin/.wxedid-wrapped
+    makeWrapper $out/bin/.wxedid-wrapped $out/bin/wxedid \
+      --suffix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}"
+  '';
 }
