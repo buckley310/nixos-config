bb93d801bc4ca98dc91c129845e9901ccb5bc044
Sean Buckley <sean.bck@gmail.com>
2025-10-02 02:46:23 -0400

    foot: scroll by half-pages

--- modules/desktop/foot.nix
+++ modules/desktop/foot.nix
@@ -4,5 +4,10 @@
   environment.etc."xdg/foot/foot.ini".text = ''
     [main]
     font=DejaVuSansMono:size=14
+    [key-bindings]
+    scrollback-up-page=none
+    scrollback-down-page=none
+    scrollback-up-half-page=Shift+Page_Up
+    scrollback-down-half-page=Shift+Page_Down
   '';
 }
