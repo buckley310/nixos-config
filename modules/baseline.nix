5332a6ca8a280b9f2d19a6728347eb17264b65a0
Sean Buckley <sean.bck@gmail.com>
2026-02-15 23:57:36 -0500

    dont deploy foot to headless systems

--- modules/baseline.nix
+++ modules/baseline.nix
@@ -76,6 +76,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    foot.terminfo
     smartmontools
     sshfs
   ];
--- modules/desktop/foot.nix
+++ modules/desktop/foot.nix
@@ -1,13 +1,20 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = [ pkgs.foot ];
-  environment.etc."xdg/foot/foot.ini".text = ''
-    [main]
-    font=DejaVuSansMono:size=14
-    [key-bindings]
-    scrollback-up-page=none
-    scrollback-down-page=none
-    scrollback-up-half-page=Shift+Page_Up
-    scrollback-down-half-page=Shift+Page_Down
-  '';
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  config = lib.mkIf (config.sconfig.desktop.enable) {
+    environment.systemPackages = [ pkgs.foot ];
+    environment.etc."xdg/foot/foot.ini".text = ''
+      [main]
+      font=DejaVuSansMono:size=14
+      [key-bindings]
+      scrollback-up-page=none
+      scrollback-down-page=none
+      scrollback-up-half-page=Shift+Page_Up
+      scrollback-down-half-page=Shift+Page_Down
+    '';
+  };
 }
